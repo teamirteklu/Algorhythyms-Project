1e253bb2b0e590cb00325809d53dacf4ba074dd9
Jacob Pickron <jpickron30@gmail.com>
2022-11-21 21:40:45 -0700

    started a fetch of the url to try and get the artits and songs realted to the search

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var websiteUrl = 'https://api.musixmatch.com/ws/1.1/'
 var requestUrl = 'https://musicbrainz.org/ws/2/'
+
+$.ajax({
+    url: requestUrl,
+    method: 'PUSH',
+  }).then(function (response) {
+    console.log('AJAX Response \n-------------');
+    console.log(response);
+  });
+
+
+function getApi() {
+    var requestUrl = ''
+
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+}
