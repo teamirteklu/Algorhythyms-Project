41367a4ca9722f884e9ef9fcb4a3768f80c5fea4
emilyy231 <Emily.Yates@du.edu>
2022-11-22 19:04:11 -0700

    Updated HTML and first fetch request in js

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-var websiteUrl = 'https://api.musixmatch.com/ws/1.1/'
-var requestUrl = 'https://musicbrainz.org/ws/2/'
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "16e36212f8msh7435d930e8ebfa3p10cd71jsn108eca6b3b92",
+    "X-RapidAPI-Host": "lyrics-plus.p.rapidapi.com",
+  },
+};
+
+var websiteUrl = "https://api.musixmatch.com/ws/1.1/";
+var requestUrl = "https://lyrics-plus.p.rapidapi.com/lyrics/";
+
+var recommendationTable = document.getElementById("search-results-table");
+
+function getLyrics() {
+  event.preventDefault;
+  let title = document.getElementById("inputTitle5").value;
+  let artist = document.getElementById("inputArtist5").value;
+  if (artist === "" || title === "") {
+    alert("Both fields must be filled out");
+  }
+  console.log(title);
+  console.log(artist);
+
+  fetch(requestUrl + title + "/" + artist, options)
+    .then((response) => response.json())
+
+    .then((response) => {
+      console.log(response.name, response.lyrics);
+      var lyricResult = response.lyrics;
+      var songName = response.name;
+
+      var recommendationTable = document.getElementById("search-results-table");
+      var parentDiv = recommendationTable.parentNode;
+
+      var LyricDisplay = document.createElement("div");
+      var textContent = document.createTextNode(lyricResult);
+      LyricDisplay.appendChild(textContent);
+
+      parentDiv.insertBefore(LyricDisplay, recommendationTable);
+    })
+
+    .catch((err) => console.error(err));
+
+  //   });
+}
