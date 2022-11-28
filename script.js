bb397c17d65e1fc9fcc2a757fe26b7043d6e7f2b
Jacob Pickron <jpickron30@gmail.com>
2022-11-27 23:22:43 -0700

    got all of the fetches for artist, song, album, and cover art

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const options = {
     "X-RapidAPI-Host": "lyrics-plus.p.rapidapi.com",
   },
 };
-
-var websiteUrl = "https://musicbrainz.org/ws/2/";
+var tempUrl = "https://musicbrainz.org/ws/2/artits/?query=davidbowie"
+// https://musicbrainz.org/ws/2/release/?query=release=iceonfire&fmt=json
+var websiteUrl = "https://musicbrainz.org/ws/2/artist/?";
 var requestUrl = "https://lyrics-plus.p.rapidapi.com/lyrics/";
 
 var submitButton = document.querySelector("#submit");
 
 var recommendationTable = document.getElementById("search-results-table");
+
 
 function getLyrics() {
   event.preventDefault;
@@ -49,17 +51,98 @@ function getLyrics() {
 
     .catch((err) => console.error(err));
 }
+submitButton.addEventListener("click", getLyrics);
 
-submitButton.addEventListbener("click", getLyrics);
+// const option = {
+//   method: "GET",
+//   headers: {
+//     "X-MusicBrainzAPI-Key": "xSnQ2rQ1yUozeTQdGNfy9rLexMAoEIIIzDeAaxsC",
+//     "X-MusicBrainzAPI-Host": "https://musicbrainz.org/ws/2/",
+//   },
+// };
 
-const option = {
-  method: "GET",
-  headers: {
-    "X-MusicBrainzAPI-Key": "xSnQ2rQ1yUozeTQdGNfy9rLexMAoEIIIzDeAaxsC",
-    "X-MusicBrainzAPI-Host": "https://musicbrainz.org/ws/2/",
-  },
+// function getArtits(artistName) {
+  // let artistUrl = `https://musicbrainz.org/ws/2/release/?query=artist=${artistName}&fmt=json`
+  // let artistName = document.getElementById("inputArtist5").value;
+ 
+//   fetch(artistUrl)
+//   .then(response => response.json())
+
+  // .then(apiResults => {
+    // console.log(apiResults);
+  // })
+
+// }
+// let artistName = document.getElementById("inputArtist5").value;
+// submitButton.addEventListener('click', getArtits(artistName));
+// console.log(artistName);
+
+// https://musicbrainz.org/ws/2/release-group/xSnQ2rQ1yUozeTQdGNfy9rLexMAoEIIIzDeAaxsC?inc=genres+user-genres&fmt=json
+// https://musicbrainz.org/ws/2/release-group/xSnQ2rQ1yUozeTQdGNfy9rLexMAoEIIIzDeAaxsC?inc=artits+artist-rels/query=artits=katyperry&fmt=json
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+    method: "get",
+    headers: myHeaders,
+    redirect: "follow"
+    
 };
 
-function getArtits() {
 
-}
+function getArtits() {
+  let artist = document.getElementById("inputArtist5").value;
+  var artistSection = document.querySelector("#artist-display");
+  if (artistSection) {
+    artistSection.innerHTML = " ";
+  }
+  let artistUrl = `https://v1.nocodeapi.com/jpick23/spotify/EGJDVuLUpbohxGCi/search?q=${artist}&type=artist`
+
+ fetch(artistUrl, requestOptions)
+
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error))
+    
+  
+};
+
+submitButton.addEventListener('click', getArtits);
+
+function getAlbums() {
+  let artist = document.getElementById("inputArtist5").value;
+  var artistSection = document.querySelector("#artist-display");
+  if (artistSection) {
+    artistSection.innerHTML = " ";
+  }
+  let artistUrl = `https://v1.nocodeapi.com/jpick23/spotify/EGJDVuLUpbohxGCi/search?q=${artist}&type=album`
+
+ fetch(artistUrl, requestOptions)
+
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error))
+    
+  
+};
+
+submitButton.addEventListener('click', getAlbums);
+
+function getTracks() {
+  let artist = document.getElementById("inputArtist5").value;
+  var artistSection = document.querySelector("#artist-display");
+  if (artistSection) {
+    artistSection.innerHTML = " ";
+  }
+  let artistUrl = `https://v1.nocodeapi.com/jpick23/spotify/EGJDVuLUpbohxGCi/search?q=${artist}&type=track`
+
+ fetch(artistUrl, requestOptions)
+
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error))
+    
+  
+};
+
+submitButton.addEventListener('click', getTracks);
